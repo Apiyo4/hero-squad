88632fc665bf0f94416a364c7c284b68733f5de9
Annette Odingo <odingoannette@gmail.com>
2019-06-23 13:29:14 +0300

    Create a fully functioning backend

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,5 +1,52 @@
 package models;
 
-public class Hero {
+import java.util.ArrayList;
 
+public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    private int id;
+    private static ArrayList<Hero> instances = new ArrayList<Hero>();
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public static void clear(){
+        instances.clear();
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public static ArrayList<Hero> getAll() {
+        return instances;
+    }
+    public static Hero findById(int id){
+        return instances.get( id - 1);
+    }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,84 @@
 package models;
 
-import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HeroTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Hero.clear();
+    }
+
+    @Test
+    public void newHero_InstantiatesCorrectly() {
+        Hero hero = new Hero("Flash", 28, "fast", "dies");
+        assertEquals(true, hero instanceof Hero);
+    }
+
+    @Test
+    public void newHero_getsName_Flash() {
+        Hero hero = setupNewHero();
+        assertEquals("Flash", hero.getName());
+    }
+
+    @Test
+    public void newHero_getsAge_28() {
+        Hero hero = setupNewHero();
+        assertEquals(28, hero.getAge());
+    }
+
+    @Test
+    public void newHero_getSpecialPower_fast() {
+        Hero hero = setupNewHero();
+        assertEquals("fast", hero.getSpecialPower());
+    }
+
+    @Test
+    public void newHero_getWeakness_dies() {
+        Hero hero = setupNewHero();
+        assertEquals("dies", hero.getWeakness());
+    }
+
+    @Test
+    public void newHero_getId_1() {
+        Hero.clear();
+        Hero hero = setupNewHero();
+        assertEquals(1, hero.getId());
+    }
+
+    @Test
+    public void newHero_returnsAllInstancesOfHeroes() {
+        Hero hero = setupNewHero();
+        Hero otherHero = new Hero("Moose", 31, "weapons", "cake");
+        assertEquals(true, Hero.getAll().contains(hero));
+        assertEquals(true, Hero.getAll(). contains(hero));
+    }
+
+    @Test
+    public void newHero_findsCorrectHero() throws Exception{
+        Hero hero = setupNewHero();
+        assertEquals(1, Hero.findById(hero.getId()).getId());
+    }
+
+    @Test
+    public void newHero_findsReturnCorrectHeroWhenMoreHeroesExists() {
+        Hero hero = setupNewHero();
+        Hero otherHero =  new Hero("Moose", 31, "weapons", "cake");
+        assertEquals(2, Hero.findById(otherHero.getId()).getId());
+    }
+
+    public Hero setupNewHero(){
+        return new Hero("Flash", 28, "fast", "dies");
+    }
 
 }
