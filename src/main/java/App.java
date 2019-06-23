45408d42820e514919ea8e908567a328d4044c9b
Annette Odingo <odingoannette@gmail.com>
2019-06-23 15:51:00 +0300

    Add template called success

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +10,29 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/heroes/new", (request, response) -> {
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.params("age"));
+            String specialPower = request.queryParams("specialPower");
+            String weakness = request.queryParams("weakness");
+            Hero hero = new Hero(name, age, specialPower, weakness);
+
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/heroes/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
