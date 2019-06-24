337d629148886bba21e1f124cc7e2709a3b09508
Annette Odingo <odingoannette@gmail.com>
2019-06-24 09:37:46 +0300

    Add success.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -10,12 +11,34 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad_form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            int max_size = Integer.parseInt(request.queryParams("max_size"));
+            String cause = request.queryParams("cause");
+            Squad squad = new Squad(max_size, name, cause);
+            model.put("squad", squad);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            ArrayList<Hero> heroes = Hero.getAll();
-            model.put("heroes", heroes);
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+
     }
-}
+    }
