f22d896a6b6feca0fad3bdd9c8a572e50845b82c
Annette Odingo <odingoannette@gmail.com>
2019-06-24 06:45:02 +0300

    Remove route from App.java aand delete some templates to accomodate Squad

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,39 +11,7 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/heroes/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "hero-form.hbs");
-        }, new HandlebarsTemplateEngine());
 
-        post("/heroes/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            String name = request.queryParams("name");
-            int age = Integer.parseInt(request.params("age"));
-            String specialPower = request.queryParams("specialPower");
-            String weakness = request.queryParams("weakness");
-            Hero newHero = new Hero(name, age, specialPower, weakness);
-            request.session().attribute("newHero", newHero);
-            model.put("hero", newHero);
-
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Hero> heroes = Hero.getAll();
-            model.put("heroes", heroes);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-
-
-        get("/heroes/:id", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfHeroToFind = Integer.parseInt(request.params("id"));
-            Hero foundHero = Hero.findById(idOfHeroToFind);
-            model.put("hero", foundHero);
-            return new ModelAndView(model, "hero-detail.hbs");
-        }, new HandlebarsTemplateEngine());
 
     }
 }
